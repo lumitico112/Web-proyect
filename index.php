3269342866c568aeb5519669ff5566c1d5e87667
Your Name <your.email@example.com>
2025-07-14 12:42:24 -0500

    Editando login

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
+/**
+ * Created by:
+ * author: @LuisMi
+ * Fecha: 2025-07-14
+ * Time: 11:43
+ */
+include ('app/config.php');
+include ('layout/parte1.php');
+?>
 
-echo "Hola mundo!";
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <br>
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+            <h1>Vista Principal</h1>
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+<?php
+
+include ('layout/paste2.php');
+
+?>
--- login/index.php
+++ login/index.php
@@ -5,4 +5,70 @@
  * Fecha: 2025-07-14
  * Time: 10:41
  */
-echo "Hola mundo!";
+include ('../app/config.php');
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Log in</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?=APP_URL;?>/public/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="<?=APP_URL;?>/public/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?=APP_URL;?>/public/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+    <center>
+        <br>
+        <img src="<?=APP_URL;?>\public\images\login.png" width="160px" alt="logo"><br><br>
+    </center>
+  <div class="login-logo">
+    <h3 href=""><b><?=APP_NAME;?></b></h3>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Inicio de sesión</p>
+      <br>
+
+      <form action="controller_login.php" method="post">
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+      </form>
+      
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="<?=APP_URL;?>/public/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?=APP_URL;?>/public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?=APP_URL;?>/public/dist/js/adminlte.min.js"></script>
+</body>
+</html>
